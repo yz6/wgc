e5df1cf37b49a8dcdfd2db93fad7061a68d967ef
杨臻 <yangzhen@lovedabai.com>
2019-10-22 19:11:21 +0800

    first commit

---
+++ dist/static/lang/en.js
@@ -0,0 +1,47 @@
+module.exports = {
+    language: '简体中文',
+    menu:'MENU',
+    home: {
+        title: 'HOME',
+        content:'Leading crypto investment bank that provides professional advisory services across the blockchain spectrum'
+    },
+    aboutUs: {
+        title: 'ABOUT US',
+        content:'Our team comes from listing departments of top crypto exchanges and top token funds. We aim to provide the most efficient services for helping top-tier blockchain projects meet the demands of first-tier exchanges.  We also provide advisory services on offering strategies and staff an in-house market making team.'
+    },
+    service: {
+        title: 'ADVISORY',
+        item1:{
+            title:'Listing Service for Top Tier Crypto Exchanges ',
+            content:'We provide a prime channel for top blockchain projects to list on top cryptocurrency exchanges.  Our cooperative exchanges include Binance, Binance DEX, Huobi, Okex, Bithumb, Upbit, etc. '
+        },
+        item2:{
+            title:'Secondary Market Advisory Service',
+            content:'We provide customer advisory service for blockchain projects on the secondary markets.  We specifically cater different tokens based on needs, ranging from economic design to different types of trading strategies/liquidity.'
+            
+        }
+    },
+    investment: {
+        title: 'INVESTMENT',
+        content:'We are always looking for innovative blockchain projects in the primary and secondary markets.  For interest, please inquire: advisory@btx.capital. ',
+        emailw:'Email：',
+        email:'advisory@btx.capital'
+    },
+    contact: {
+        title: 'CONTACT US',
+        content:'please contact',
+        weChat:'WeChat',
+        email:'advisory@btx.capital',
+        formW:'docs.google.com',
+        form:{
+            title:['If you need our service ', 'please click'],
+            exchange:'需要申请的交易所',
+            budget:'申请交易所的预算',
+            website:'项目官网',
+            profile:'项目简介',
+            type:'代币类型',
+            contacts:'项目联系人',
+        }
+    }
+    
+}
